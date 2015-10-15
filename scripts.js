49094120c07fc4f0dade126831572976f95f9cec
sltiedeman <sltiedeman@gmail.com>
2015-10-14 23:24:56 -0400

    Creating a blackjack table with CSS
    Initial board created using HTML & CSS.  Most Javascript work to be
    added later.

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function deal(){
+	var deck =[];
+	//fill our deck, in order(for now)
+	//suit
+	var suit = "";
+	for(s = 1; s <= 4; s++){
+		if(s===1){
+			suit = "h";
+		}else if(s === 2){
+			suit = "s";
+		}else if(s === 3){
+			suit = "d";
+		}else if(s === 4){
+			suit = "c";
+		}
+		//card number
+		for(i=1; i <= 13; i++){
+			deck.push(i+suit);
+		}
+	}
+	console.log(deck);
+
+	var numberOfTimesToShuffle = Math.floor(Math.random() * 500 + 500);
+	//Shuffle the deck
+	for(i = 0; i < numberOfTimesToShuffle; i++){
+		//Pick 2 random cards from the deck.  And switch them.
+		var card1 = Math.floor(Math.random()*52);
+		var card2 = Math.floor(Math.random()*52);
+		var temp = deck[card2];
+		deck[card2] = deck[card1];
+		deck[card1] = temp;
+	}
+	console.log(deck);
+}
